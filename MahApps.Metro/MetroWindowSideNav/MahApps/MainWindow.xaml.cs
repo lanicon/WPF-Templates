39886d5b280f5b88478a52b040b763649b96b5ff
fjgruenewald <61251684+fj-gruenewald@users.noreply.github.com>
2020-11-11 17:38:09 +0100

    New Project Structure

---
+++ MahApps.Metro/MetroWindowSideNav/MahApps/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MahApps.Metro.Controls;
+
+namespace MahApps
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
